3b489238f86422666191e78c2809688ca7d9cc55
Артём Иванов <fast0490f@mail.ru>
2022-02-09 20:21:14 +0300

    version 5.7.0

---
+++ test3.js
@@ -0,0 +1,20 @@
+const Rtsp = require('./lib/rtsp');
+
+
+const rtsp = new Rtsp({
+  "chan": "cam_1",
+  "name":"Cam1",
+  "url": '',
+  "type": "rtsp/h264",
+  "protocol": "tcp",
+  "transport": "p2p",
+  "comment": "",
+  "check_pps": false,
+});
+  
+rtsp.on('play', (i) => console.log(i));
+rtsp.on('stream', (i) => {});
+rtsp.on('close', () => console.log('close'));
+rtsp.on('debug', (i) => {});
+rtsp.on('error', (e) => console.log('error', e));
+  
